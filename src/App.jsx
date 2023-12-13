e97dde8024e4b2f4429a27bdc86375ec7569df98
Josip Milan <josip.milan@aol.com>
2023-12-13 16:39:11 +0100

    master

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,27 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
 
 function App() {
-
   return (
-    <div className="App relative z-20 pt-20">
-      <Navbar />
+    <Router>
+      <div className="App relative z-20 pt-20">
+        <Navbar />
 
-      <div className="pages">
-        <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/students/:studentId"
+              element={<StudentDetailsPage />}
+            />
+            <Route path="/profile" element={<UserProfilePage />} />
+          </Routes>
+        </div>
       </div>
-
-    </div>
+    </Router>
   );
 }
 
